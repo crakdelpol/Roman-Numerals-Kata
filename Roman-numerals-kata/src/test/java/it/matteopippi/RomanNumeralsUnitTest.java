f6c62a206bf9230127f4bb6f387e45d373180094
Matteo Pipitone <matteo.pipitone@mdotm.ai>
2025-03-04 19:51:59 +0100

    setup

---
+++ Roman-numerals-kata/src/test/java/it/matteopippi/RomanNumeralsUnitTest.java
@@ -0,0 +1,13 @@
+package it.matteopippi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RomanNumeralsUnitTest {
+
+    @Test
+    void testaaaa() {
+        fail();
+    }
+}
